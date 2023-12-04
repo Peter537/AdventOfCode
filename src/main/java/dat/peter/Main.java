61aa3333cabb1619ee93b3171a4fee8a51c61cb1
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-12-04 11:55:18 +0100

    made a little menu for Main

--- src/main/java/dat/peter/Main.java
+++ src/main/java/dat/peter/Main.java
@@ -6,16 +6,44 @@ import dat.peter.day3.Day3;
 import dat.peter.day4.Day4;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
+    private final List<Day> DAY_MAP = List.of(
+            new Day1(),
+            new Day2(),
+            new Day3(),
+            new Day4()
+    );
+
     public static void main(String[] args) {
+        new Main().run();
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
         List<Day> days = new ArrayList<>();
-        days.add(new Day1());
-        days.add(new Day2());
-        days.add(new Day3());
-        days.add(new Day4());
+        while (true) {
+            System.out.println("All answers: a(ll)");
+            System.out.println("Otherwise type numbers like: 1 3 4");
+            String answer = scanner.nextLine().toLowerCase();
+            if (answer.equals("a") || answer.equals("all")) {
+                days.addAll(DAY_MAP);
+                break;
+            } else {
+                try {
+                    days.addAll(Arrays.stream(answer.split(" "))
+                            .map(Integer::parseInt)
+                            .map(index -> DAY_MAP.get(index - 1))
+                            .toList());
+                    break;
+                } catch (Exception ignored) {
+                }
+            }
+        }
 
         days.forEach(Day::run);
     }
