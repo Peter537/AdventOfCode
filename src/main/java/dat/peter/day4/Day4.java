b6e11286349bcd4e64b6018f8b4e70b26413fecf
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-12-04 09:19:54 +0100

    fixed part2 day4

--- src/main/java/dat/peter/day4/Day4.java
+++ src/main/java/dat/peter/day4/Day4.java
@@ -87,48 +87,37 @@ public class Day4 extends Day {
     @Override
     public String partTwo(List<String> input) {
         int sum = 0;
-//        input = new ArrayList<>();
-//        input.add("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
-//        input.add("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19");
-//        input.add("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1");
-//        input.add("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83");
-//        input.add("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36");
-//        input.add("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11");
-        Map<Integer, List<Card>> cardMap = new LinkedHashMap<>();
+        Map<Integer, Card> cardMap = new LinkedHashMap<>();
 
         for (String line : input) {
-//            String[] split1 = line.split(": ");
-//            String[] split2 = split1[1].split(" \\| ");
-//            List<Integer> winningNumbers = getNumbers(split2[0]).stream().sorted().toList();
-//            List<Integer> myNumbers = getNumbers(split2[1]).stream().sorted().toList();
             Card card = Card.fromLine(line);
-            List<Card> cards = new LinkedList<>();
-            cards.add(card);
-            cardMap.put(card.cardNumber(), cards);
+            cardMap.put(card.cardNumber(), card);
         }
 
         Map<Integer, Integer> timesMap = new HashMap<>();
+        Map<Integer, Integer> cardTimesMap = new HashMap<>();
         for (int cardNumber : cardMap.keySet()) {
-            List<Card> cards = cardMap.get(cardNumber);
-            System.out.println("Going through: " + cardNumber + "(" + cards.size() + ")");
-            for (Card card : cards) {
-                int times = timesMap.getOrDefault(cardNumber, -1);
-                if (times == -1) {
-                    times = getTimes(card.winningNumbers(), card.myNumbers());
-                    timesMap.put(cardNumber, times);
-                }
-//                int times = getTimes(card.winningNumbers(), card.myNumbers());
-//                System.out.println(card.cardNumber);
-                sum += 1;
-//                System.out.println("times: " + times);
-//                cardMap.values().forEach(System.out::println);
-                for (int i = 1; i <= times; i++) {
-                    List<Card> cards1 = cardMap.get(cardNumber + i);
-                    List<Card> newList = new ArrayList<>(cards1);
-                    newList.add(cards1.get(0));
-                    cardMap.put(cardNumber + i, newList);
-                }
+            Card card = cardMap.get(cardNumber);
+            int times = timesMap.getOrDefault(cardNumber, -1);
+            if (times == -1) {
+                times = getTimes(card.winningNumbers(), card.myNumbers());
+                timesMap.put(cardNumber, times);
             }
+
+            int currentTimes = cardTimesMap.getOrDefault(cardNumber, -1);
+            if (currentTimes == -1) {
+                cardTimesMap.put(cardNumber, 1);
+                currentTimes = cardTimesMap.get(cardNumber);
+            }
+
+            for (int i = 1; i <= times; i++) {
+                int nextCardNumber = cardNumber + i;
+                cardTimesMap.put(nextCardNumber, cardTimesMap.getOrDefault(nextCardNumber, 1) + (currentTimes));
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : cardTimesMap.entrySet()) {
+            sum += entry.getValue();
         }
 
         return String.valueOf(sum);
