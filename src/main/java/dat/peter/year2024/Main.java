a0919731a7b3088b310fa4ea15e90d8f6847cf68
Peter537 <pean@outlook.dk>
2024-12-02 18:25:03 +0100

    Create Main.java

---
+++ src/main/java/dat/peter/year2024/Main.java
@@ -0,0 +1,47 @@
+package dat.peter.year2024;
+
+import dat.peter.Day;
+import dat.peter.year2024.day1.Day1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private final List<Day> DAY_MAP = List.of(
+            new Day1()
+    );
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        List<Day> days = new ArrayList<>();
+        while (true) {
+            System.out.println("All answers: a(ll)");
+            System.out.println("Otherwise type numbers like: 1 3 4");
+            String answer = scanner.nextLine().toLowerCase();
+            if (answer.equals("a") || answer.equals("all")) {
+                days.addAll(DAY_MAP);
+                break;
+            } else {
+                try {
+                    days.addAll(Arrays.stream(answer.split(" "))
+                            .map(Integer::parseInt)
+                            .map(index -> DAY_MAP.get(index - 1))
+                            .toList());
+                    break;
+                } catch (Exception ignored) {
+                }
+            }
+        }
+
+        days.forEach(day -> {
+            day.run(2024);
+        });
+    }
+}
